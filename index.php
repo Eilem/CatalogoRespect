abc0dd56ba5ca3365a7a2f297ad657677bd3a524
Eilem <eilem.rei@gmail.com>
2015-03-03 03:03:32 -0300

    inseriondo campos da mercadoria

--- index.php
+++ index.php
@@ -10,14 +10,11 @@ $router = new Respect\Rest\Router();
 
 $router->get("/mercadoria/*" , function( $id) use($mapper){ 
     $m = $mapper->mercadoria[$id]->fetch();
-    
     return json_encode($m);
 });
 
 $router->get("/mercadoria/criar", function(){
-
     return file_get_contents("./criar.html");
-    
 });
 
 
@@ -27,9 +24,20 @@ $router->post("/mercadoria", function() use($mapper){
 //recebo data do ajax
 	$mercadoria = new stdClass();
     $mercadoria->id_book = $_POST['book'];
+    
+    $mercadoria->tipo = $_POST['tipo'];
+    $mercadoria->codigo = $_POST['codigo'];
+
     $mercadoria->nome = $_POST['nome'];
     $mercadoria->titulo = $_POST['titulo'];
     $mercadoria->descricao = $_POST['descricao'];
+
+    $mercadoria->detalhes = $_POST['detalhes'];
+
+    $mercadoria->preco = $_POST['preco'];
+    $mercadoria->preco_promocional = $_POST['precoPromocional'];
+    $mercadoria->status = $_POST['status'];
+
     $mercadoria->imagem = $_POST['imagem'];
 
     $mapper->mercadoria->persist($mercadoria);
